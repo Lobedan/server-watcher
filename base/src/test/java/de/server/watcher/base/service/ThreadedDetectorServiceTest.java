9ea3e4d9fa85e51bf245661287d79691d43d59c3
sven klemmer <svenklemmer@aol.com>
2014-11-06 15:31:50 +0100

    added qualifier in bean for right service

--- base/src/test/java/de/server/watcher/base/service/ThreadedDetectorServiceTest.java
+++ base/src/test/java/de/server/watcher/base/service/ThreadedDetectorServiceTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -22,6 +23,7 @@ import de.server.watcher.base.detector.OsDetector;
 public class ThreadedDetectorServiceTest {
 
   @Autowired
+  @Qualifier("threadedDetectorService")
   private DetectorService service;
 
   @Test
